4266ceeb5bf70c11cfaa089c1da36191b739d157
damarsierra <55456186+damarsierra@users.noreply.github.com>
2020-12-29 13:35:45 -0500

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from pymongo import MongoClient
 
 def create_app():
     app = Flask(__name__)
-    client = MongoClient("mongodb+srv://damar:ddd333@microblog-application.syqs0.mongodb.net/test")
+    client = MongoClient("mongodb+srv://damar:ddd333@microblog-application.syqs0.mongodb.net/microblog?retryWrites=true&w=majority")
     app.db = client.microblog
 
     @app.route("/", methods=["GET", "POST"])
